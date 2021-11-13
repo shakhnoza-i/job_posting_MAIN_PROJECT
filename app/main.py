8bc3179b7169c67ba0a9487b14b4ae6765636631
shaha <shaha0111@mail.ru>
2021-11-13 11:43:41 +0600

    3 commit

--- app/main.py
+++ app/main.py
@@ -2,13 +2,19 @@ from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
 from db.base import Base
+from apis.base import api_router
 
 def create_tables(): # connects our Fast API with the database engine.
     Base.metadata.create_all(bind=engine) 
 
+def include_router(app):
+    app.include_router(api_router)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 app = start_application()
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -6,3 +6,12 @@ class UserCreate(BaseModel):
     username : str 
     email : EmailStr
     password : str 
+
+
+class ShowUser(BaseModel): # like serializer
+    username : str 
+    email : EmailStr
+    is_active : bool 
+
+    class Config(): # convert to dictionary type
+        orm_mode = True
